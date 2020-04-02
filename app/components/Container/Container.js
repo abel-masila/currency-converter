e271f545061e2ed6700dc1619b9f3d0f13158f77
Abel <abelmasila@gmail.com>
2020-04-02 21:25:49 +0300

    added  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}> to be able to close keyboard

--- app/components/Container/Container.js
+++ app/components/Container/Container.js
@@ -1,11 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { View } from "react-native";
+import { View, TouchableWithoutFeedback, Keyboard } from "react-native";
 
 import styles from "./styles";
 
 const Container = ({ children }) => (
-  <View style={styles.container}>{children}</View>
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    <View style={styles.container}>{children}</View>
+  </TouchableWithoutFeedback>
 );
 Container.propTypes = {
   children: PropTypes.any
